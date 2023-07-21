2490832c6a4f2469ed8994106ea9c39226a3fe95
mursalin@gmail.com <MURSA>
2023-07-21 14:42:20 +0600

    almost ready goint for final test

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Form, Button, Container, Row, Col, Modal } from "react-bootstrap";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import Lottie from "react-lottie";
-import animationData from "/public/signup.json";
+import animationData from "/public/signup.json"; // Remove "/public" from the path
 import "./Login.css";
 import { BsFacebook } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 import Signup from "../Signup/Signup";
+import { AuthContext } from "../Provider/AuthProvider";
+import { GoogleAuthProvider } from "firebase/auth"; 
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const { signIn, signInGoogle } = useContext(AuthContext);
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -28,6 +36,7 @@ const Login = () => {
   });
 
   const [showModal, setShowModal] = useState(false);
+  const [showSuccessModal, setShowSuccessModal] = useState(false); // State variable for success modal
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -47,11 +56,55 @@ const Login = () => {
     setShowModal(true);
   };
 
+  const handleSignInSuccess = () => {
+    setShowSuccessModal(true); // Show the success modal
+    setFormData({ // Reset the form data
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
+  const handleSignInForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signIn(email, password)
+      .then((result) => {
+        const loggedInUser = result.user;
+        form.reset();
+        console.log(loggedInUser);
+        handleSignInSuccess(); // Call the function to handle sign-in success
+        navigate(from); // Navigate to the '/' route
+      })
+      .catch((error) => {
+        console.error("Your email or password not valid", error);
+      });
+  };
+
+  // Google Sign In here >>>>>>>>>>
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInGoogle(googleProvider)
+      .then((result) => {
+        const theUser = result.user;
+        console.log(theUser);
+        navigate(from);
+      })
+      .catch((error) => {
+        console.error("Error signing in with Google", error);
+      });
+  };
+
   return (
     <Container className="signup-body">
       <Row className="justify-content-center ">
         <Col xs={12} sm={10} md={8} lg={6}>
-          <Form onSubmit={handleSubmit}>
+          <Form onSubmit={handleSignInForm}>
             <div>
               <p className="fs-4 font-weight-bolder">Sign In</p>
             </div>
@@ -89,7 +142,7 @@ const Login = () => {
                 variant="primary"
                 className="w-100 mt-3 rounded-5 mb-5 d-none d-md-block d-lg-block"
               >
-                Create Account
+                Sign In
               </Button>
 
               <div className="d-flex align-items-center gap-5 d-block d-sm-none">
@@ -98,7 +151,7 @@ const Login = () => {
                   variant="primary"
                   className="w-50 mt-3 rounded-5 mb-5"
                 >
-                  Create Account
+                  Sign In
                 </Button>{" "}
                 <p className="w-50 mb-4" onClick={handleShowModal}>
                   or create account
@@ -107,10 +160,10 @@ const Login = () => {
             </div>
             <div className="text-center mb-3 d-none">Or sign up with:</div>
             <Button variant="outline-primary" className="mb-2 w-100">
-              <BsFacebook /> Sign up with Facebook
+              <BsFacebook /> Sign in with Facebook
             </Button>
-            <Button variant="outline-danger" className="w-100">
-              <FcGoogle /> Sign up with Google
+            <Button variant="outline-danger" className="w-100" onClick={handleGoogleSignIn}>
+              <FcGoogle /> Sign in with Google
             </Button>
           </Form>
         </Col>
@@ -146,6 +199,31 @@ const Login = () => {
         <Modal.Body>
           <Signup />
         </Modal.Body>
+      </Modal>
+
+      {/* Modal for Sign In Success */}
+      <Modal
+        show={showSuccessModal}
+        onHide={() => setShowSuccessModal(false)}
+        centered
+        dialogClassName="custom-modal-dialog"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title className="text-success fs-6">
+            Sign In Successful!
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Congratulations! You have successfully signed in.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => setShowSuccessModal(false)}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
       </Modal>
     </Container>
   );
--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -1,6 +1,6 @@
 // NavigationBar.js
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {
   Navbar,
   Nav,
@@ -18,8 +18,10 @@ import {
 } from "react-icons/bs";
 import Signup from "../Signup/Signup";
 import "./NavigationBar.css";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const NavigationBar = () => {
+  const { signOutUser, user } = useContext(AuthContext);
   const [showModal, setShowModal] = useState(false);
 
   const handleCreateAccount = () => {
@@ -29,7 +31,14 @@ const NavigationBar = () => {
   const handleCloseModal = () => {
     setShowModal(false);
   };
-
+  console.log(user);
+  const signOutHandler = () => {
+    signOutUser()
+      .then((result) => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <React.Fragment>
       <Navbar bg="white" variant="light" expand="lg" className="shadow">
@@ -64,24 +73,61 @@ const NavigationBar = () => {
                 className="rounded-pill text-center  pr-3 bg-danger-subtle"
               />
             </Form>
-            <Nav>
-              <Nav.Link
-                href="#create-account"
-                className="text-dark font-weight-bold"
-                onClick={handleCreateAccount}
-              >
-                Create Account, <span className="text-primary">It's Free</span>
-                <span className="ml-2">▼</span>
-              </Nav.Link>
-            </Nav>
+            <div>
+              {user ? (
+                <div className="d-flex  gap-3">
+                  <img
+                    className="profile-photo"
+                    src="https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFkfGVufDB8fDB8fHww&w=1000&q=80"
+                    alt=""
+                  />
+                  <p
+                    className="bg-link-light font-weight-bold mt-2"
+                    style={{ color: "black" }}
+                  >
+                    {user?.displayName ? user.displayName : user?.email}
+                  </p>
+                </div>
+              ) : (
+                <Nav>
+                  <Nav.Link
+                    href="#create-account"
+                    className="text-dark font-weight-bold"
+                    onClick={handleCreateAccount}
+                  >
+                    Create Account,{" "}
+                    <span className="text-primary">It's Free</span>
+                    <span className="ml-2">▼</span>
+                  </Nav.Link>
+                </Nav>
+              )}
+            </div>
+            <div>
+              {user ? (
+                <button className="ml-3 btn btn-outline-info" onClick={signOutHandler}>
+                  {" "}
+                  Singout
+                </button>
+              ) : (
+                " "
+              )}
+            </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
       {/* Create Account Modal */}
-      <Modal show={showModal} onHide={handleCloseModal} centered dialogClassName="custom-modal-dialog">
+      <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        centered
+        dialogClassName="custom-modal-dialog"
+      >
         <Modal.Header closeButton>
-          <Modal.Title className="text-success fs-6 ">Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</Modal.Title>
+          <Modal.Title className="text-success fs-6 ">
+            Let's learn, share & inspire each other with our passion for
+            computer engineering. Sign up now 🤘🏼
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body dialogClassName="custom-modal-dialog2">
           <Signup />
